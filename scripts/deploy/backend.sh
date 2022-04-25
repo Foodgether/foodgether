af5067ae17349109b2c72a93ab96b2ff82617604
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-04-25 12:15:22 +0700

    Add environment variable to stop jenkins ending process

--- scripts/deploy/backend.sh
+++ scripts/deploy/backend.sh
@@ -3,4 +3,4 @@ sudo mkdir /home/ubuntu/foodgether_build/server
 
 sudo cp -R ./server/dist/* /home/ubuntu/foodgether_build/server
 ls /home/ubuntu/foodgether_build/server
-pm2 start /home/ubuntu/foodgether_build/server/app.js --name=server
+BUILD_ID=dontKillMe JENKINS_NODE_COOKIE=dontKillMe pm2 start /home/ubuntu/foodgether_build/server/app.js --name=server
--- scripts/deploy/frontend.sh
+++ scripts/deploy/frontend.sh
@@ -2,4 +2,4 @@ sudo rm -rf /home/ubuntu/foodgether_build/frontend
 sudo mkdir /home/ubuntu/foodgether_build/frontend
 
 sudo cp -R ./frontend/dist /home/ubuntu/foodgether_build/frontend
-pm2 serve /home/ubuntu/foodgether_build/frontend 8181 --name=frontend --spa
+BUILD_ID=dontKillMe JENKINS_NODE_COOKIE=dontKillMe pm2 serve /home/ubuntu/foodgether_build/frontend 8181 --name=frontend --spa
