c3bd8bbbf080f753b1fa3f320b2463b4f4850190
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-04-30 17:39:50 +0700

    Use External Chrome

--- server/src/scraper/browser.ts
+++ server/src/scraper/browser.ts
@@ -5,11 +5,21 @@ export const startBrowser = async (): Promise<Browser | null> => {
   let browser;
   try {
     logger.log('info', 'Opening the browser......');
-    browser = await puppeteer.launch({
-      headless: true,
-      args: ['--disable-setuid-sandbox'],
-      ignoreHTTPSErrors: true,
-    });
+    if (process.env.NODE_ENV === 'development') {
+      browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 100,
+        devtools: true,
+      });
+    } else {
+      browser = await puppeteer.launch({
+        headless: true,
+        args: ['--disable-setuid-sandbox'],
+        ignoreHTTPSErrors: true,
+        executablePath: '/usr/bin/google-chrome',
+      });
+    }
+
     return browser;
   } catch (err) {
     logger.log('info', 'Could not create a browser instance => : ', err);
