a8feaa1ecaadab46e49fd47dbec96d96e2e6d8b5
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-04-30 17:47:54 +0700

    Try using chromium

--- server/src/scraper/browser.ts
+++ server/src/scraper/browser.ts
@@ -5,21 +5,11 @@ export const startBrowser = async (): Promise<Browser | null> => {
   let browser;
   try {
     logger.log('info', 'Opening the browser......');
-    if (process.env.NODE_ENV === 'development') {
-      browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 100,
-        devtools: true,
-      });
-    } else {
-      browser = await puppeteer.launch({
-        headless: true,
-        args: ['--disable-setuid-sandbox'],
-        ignoreHTTPSErrors: true,
-        executablePath: '/usr/bin/google-chrome',
-      });
-    }
-
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--disable-setuid-sandbox'],
+      ignoreHTTPSErrors: true,
+    });
     return browser;
   } catch (err) {
     logger.log('info', 'Could not create a browser instance => : ', err);
