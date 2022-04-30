5026d756726436bffb2480da9bc82574c21f9e1a
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-04-30 18:02:07 +0700

    Specify browser path

--- server/src/scraper/browser.ts
+++ server/src/scraper/browser.ts
@@ -5,11 +5,15 @@ export const startBrowser = async (): Promise<Browser | null> => {
   let browser;
   try {
     logger.log('info', 'Opening the browser......');
-    browser = await puppeteer.launch({
+    const options = {
       headless: true,
       args: ['--disable-setuid-sandbox'],
       ignoreHTTPSErrors: true,
-    });
+    };
+    if (process.env.NODE_ENV !== 'development') {
+      options["executablePath"] = '/usr/bin/chromium';
+    }
+    browser = await puppeteer.launch();
     return browser;
   } catch (err) {
     logger.log('info', 'Could not create a browser instance => : ', err);
