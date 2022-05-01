ba461bfd33ea253ba0c2be68d819a52c60f41f82
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-05-01 17:07:52 +0700

    Adjust containers stop command

--- scripts/deploy/stop.sh
+++ scripts/deploy/stop.sh
@@ -1,4 +1,5 @@
 docker rm --force foodgether-frontend
 docker rm --force foodgether-backend
 docker-compose down
+docker rm $(docker ps -aqf name="foodgether*")
 docker rmi $(docker images --filter=reference="foodgether-*" -q)
