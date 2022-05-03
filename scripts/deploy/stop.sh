e269daa6dfdeb62bba05c23cc3475b1ebade0b12
Lam Quang Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2022-05-03 11:15:46 +0700

    Add stop before removing

--- scripts/deploy/stop.sh
+++ scripts/deploy/stop.sh
@@ -1,5 +1,6 @@
 docker rm --force foodgether-frontend
 docker rm --force foodgether-backend
 docker-compose down
+docker stop $(docker ps -aqf name="foodgether*")
 docker rm $(docker ps -aqf name="foodgether*")
 docker rmi $(docker images --filter=reference="foodgether-*" -q)
